dae7bab9213a8a26a44c2bf700a07425e4eee34a
Brian Karanja <bmkaranja001@gmail.com>
2025-12-01 18:27:29 +0300

    Update:made teh top part of the landing page,changed up image

--- HireNear/src/components/LandingPage.jsx
+++ HireNear/src/components/LandingPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import NavBar from './NavBar'
+import { Link} from 'react-router-dom'
 
 function LandingPage() {
   return (
-    <div className='w-screen h-screen '>
-        <section className='  w-screen h-120 top '>
+    <div className='w-screen h-screen bar '>
+        <section className=' mr-0 w-screen h-130 top ' style={{marginRight:-20}}>
         <NavBar/>
+        <article div className='grid grid-cols-2 gap-20 mt-20'>
+            <div className='div-1'>
+                <p className='text-6xl txt-1 pl-15'>
+                  HireNear — where
+                  employers hire 
+                  and seekers get hired
+                  </p>
+                <div className='flex float-left gap-8'>
+                  <button className='bg-blue-600 hover:bg-blue-900 text-white font-semibold py-1.5 px-6 rounded-lg ml-20 mt-8'>
+                      <Link to='/jobs'>Job opportunities →</Link>
+                  </button>
+                  <button className='bg-white hover:bg-blue-900 text-blue-600 font-semibold py-1.5 px-4 rounded-lg ml-20 mt-8'>
+                      <Link to ='/hire'>Hire Talent →</Link>
+                  </button>
+                </div>
+            </div>
+            <img src='Job_searching-removebg-preview.png' className='h-90' id='searching'></img>
+        </article>
         </section>
     </div>
   )
--- HireNear/src/components/NavBar.jsx
+++ HireNear/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ function NavBar() {
           </div>
           <div className='flex flex-right w-auto gap-10'>
             <p className='l-1 text-white mt-0.5 pr-7'><Link to='/jobs' className='Links'>Looking for a Job?</Link> / <Link to ='/hire'className='Links'>Looking to Hire?</Link></p>
-            <button className='rounded-lg w-20.5 h-8 mr-2 bg-blue-600 hover:bg-blue-900 '>
+            <button className='rounded-lg w-20.5 h-8 bg-blue-600 hover:bg-blue-900 '>
               <Link to = '/signup ' className='text-white'> SignUp</Link>
             </button>
           </div>
