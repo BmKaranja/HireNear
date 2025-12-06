25a161ef851a3269c44730dbc2922562bbd8547b
Brian Karanja <bmkaranja001@gmail.com>
2025-12-06 19:18:46 +0300

    Update:styled up the cards a bit

--- HireNear/src/components/JobCards.jsx
+++ HireNear/src/components/JobCards.jsx
@@ -2,27 +2,25 @@ import React from 'react'
 
 function JobCard() {
   return (
-    <div className='grid grid-cols-2 marginr gap-50 mt-10'>
-      <div className='flex flex-1 flex-row h-10 jobcard'>
+    <div className='grid grid-cols-2 marginr gap-30 mt-10'>
+      <div className='flex flex-1 flex-row h-10 jobcards'>
         <div>
           <img src="account.png" alt="employer" className='h-20'/>
         </div>
-        <div>
+        <div className='jdetails'>
           <p>Employer Name</p>
           <p>Job Name</p>
           <p>Short description</p>
-          <p>Rating</p>
         </div>
       </div>
-            <div className='flex flex-1 flex-row h-10 shadow-amber-500'>
+      <div className='flex flex-1 flex-row h-10 jobcards'>
         <div>
           <img src="account.png" alt="employer" className='h-20'/>
         </div>
-        <div>
+        <div className='jdetails'>
           <p>Employer Name</p>
           <p>Job Name</p>
           <p>Short description</p>
-          <p>Rating</p>
         </div>
       </div>
 
--- HireNear/src/components/Searchbar.jsx
+++ HireNear/src/components/Searchbar.jsx
@@ -4,6 +4,7 @@ function Searchbar() {
   return (
     <div>
       <input type='search' placeholder='🔍 Search for your dream job.....' id='jobsearch' className='shadow-blue-950'></input><br></br><br></br>
+      <p className='underline'>Job Filter</p><br></br>
       <input type='checkbox'></input><label> Boy</label><br></br><br></br>
     </div>
   )
