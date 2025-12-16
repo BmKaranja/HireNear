11a64f79b3d54ad1e76c50401dd78970fa2dfa55
Brian Karanja <bmkaranja001@gmail.com>
2025-12-16 16:54:41 +0300

    Update:renamed my paths for render

--- HireNear/src/components/WorkInProgress.jsx
+++ HireNear/src/components/WorkInProgress.jsx
@@ -5,7 +5,7 @@ function WorkInProgress() {
   return (
     <div className='mt-30 mb-0 w-screen bar'>
       <div  style={{display:'flex', flexDirection:'column', gap:10,justifyContent:'center', alignItems:'center',alignContent:'center', marginBottom:'10vh'}} className='mb-30'>
-      <img src='C:\Users\medil\Alx_projects\HireNear\HireNear\OIP-removebg-preview.png'></img>
+      <img src='/OIP-removebg-preview.png'></img>
       <button className='bg-blue-800 rounded-xl pr-4 pl-4 text-white mt-8 pt-3 pb-3'>
         <Link to ='/landingpage'>
         Back to Home →
