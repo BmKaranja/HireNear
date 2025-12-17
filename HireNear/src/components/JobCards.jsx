98f13ffba8c2779635b400c3187ceea27065192d
Brian Karanja <bmkaranja001@gmail.com>
2025-12-17 10:41:43 +0300

    Update:Added jobs api

--- HireNear/src/components/JobCards.jsx
+++ HireNear/src/components/JobCards.jsx
@@ -1,47 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function JobCard() {
+  const [jobs, setJobs] =useState()
+  useEffect(()=>(
+    fetch ('https://findwork.dev/api/jobs/')
+    .then(res =>res.json())
+    .then(data => setJobs(data.results))
+  ))
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 marginr gap-10 mt-10'>
+      {jobs.map((job)=>(
       <div className='flex flex-1 flex-row h-10 jobcards'>
         <div className='pl-4 pr-4'>
-          <img src="account.png" alt="employer" className='h-20 w-20 mt-5 mr-2'/>
+          <img src={jobs.logo} alt="employer" className='h-20 w-20 mt-5 mr-2'/>
         </div>
         <div className='jdetails'>
-          <p>Job Name</p>
-          <p>Employer Name</p>
-          <p className='text-gray-500'>Location</p>
-          <p className='text-gray-500'>Rating</p>
+          <p>{job.role}</p>
+          <p>{job.company_name}</p>
+          <p className='text-gray-500'>{job.location}</p>
+          <p className='text-gray-500'>{job.date_posted}</p>
           <button className='bg-blue-500 text-white px-3 py-0.5 rounded mt-1 hover:bg-blue-600 transition duration-300'>Apply →</button>
         </div>
       </div>
-      <div className='flex flex-1 flex-row h-10 jobcards'>
-        <div className='pl-4 pr-4'>
-          <img src="/account.png" alt="employer" className='h-20 w-20 mt-5 mr-2'/>
-        </div>
-        <div className='jdetails'>
-          <p>Job Name</p>
-          <p>Employer Name</p>
-          <p className='text-gray-500'>Location</p>
-          <p className='text-gray-500'>Rating</p>
-          <button className='bg-blue-500 text-white px-3 py-0.5 rounded mt-1 hover:bg-blue-600 transition duration-300'>Apply →</button>
-        </div>
-      </div>
-
-      <div className='flex flex-1 flex-row h-10 jobcards gap-1'>
-        <div className='pl-4 pr-4'>
-          <img src="/account.png" alt="employer" className='h-20 w-20 mt-5 mr-2'/>
-        </div>
-        <div className='jdetails'>
-          <p>Job Name</p>
-          <p>Employer Name</p>
-          <p className='text-gray-500'>Location</p>
-          <p className='text-gray-500'>Rating</p>
-          <button className='bg-blue-500 text-white px-3 py-0.5 rounded mt-1 hover:bg-blue-600 transition duration-300'>Apply →</button>
-        </div>
-      </div>
-
-    </div>
+))}
+</div>
   )
 }
 
